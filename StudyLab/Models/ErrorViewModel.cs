8df6d012699735c0e35885788f65c301cb35528f
Deivison <deivisoneditordevideos@gmail.com>
2023-04-12 21:20:31 -0300

    Início do projeto

---
+++ StudyLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudyLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
