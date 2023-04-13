91b4c4dcce6804de193340bb5e8656c742710580
Deivison <deivisoneditordevideos@gmail.com>
2023-04-12 21:43:42 -0300

    Início de AuthenticationController

---
+++ StudyLab/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyLab.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
