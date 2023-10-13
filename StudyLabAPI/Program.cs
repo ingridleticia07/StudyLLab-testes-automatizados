12cc70b978dbedd898fe9b366183fe5d3791671b
LuanRoger <luan.roger.2003@gmail.com>
2023-10-13 18:03:47 -0300

    refactor: group DI services

--- StudyLabAPI/Program.cs
+++ StudyLabAPI/Program.cs
@@ -1,21 +1,8 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
-using StudyLabAPI.Context;
-using StudyLabAPI.Controllers;
 using StudyLabAPI.Endpoints;
-using StudyLabAPI.Filters;
-using StudyLabAPI.Middlewares.Auth;
 using StudyLabAPI.Middlewares.Cache;
-using StudyLabAPI.Middlewares.Swagger;
-using StudyLabAPI.Repositories;
-using StudyLabAPI.Services.Configuration;
-using StudyLabAPI.Services.Email;
-using StudyLabAPI.Services.Jwt;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using StudyLabAPI.Middlewares.Filters;
+using StudyLabAPI.Services;
 using ILogger = Serilog.ILogger;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -29,90 +16,16 @@ builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(serilog);
 builder.Host.UseSerilog(serilog);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>((provider, optionsBuilder) =>
-{
-    ILogger? logger = provider.GetService<ILogger>();
-    
-    string? connectionString = builder.Configuration.GetConnectionString("PostgresDbConnString");
-    if(string.IsNullOrEmpty(connectionString))
-        logger?.Warning("String de conexão com o banco de dados não especificada");
-    
-    optionsBuilder.UseNpgsql(connectionString);
-});
-builder.Services.AddSingleton<JwtService>(provider =>
-{
-    ILogger? logger = provider.GetService<ILogger>();
-    
-    JwtParametersOptions? jwtOptions = 
-    builder.Configuration
-        .GetSection(JwtParametersOptions.JWT_PARAMETERS)
-        .Get<JwtParametersOptions>();
-    if(jwtOptions is null)
-    {
-        logger?.Warning("Parâmetros do JWT não especificados");
-        jwtOptions = new();
-    }
-    
-    return new(jwtOptions.privateKey, jwtOptions.issuer, jwtOptions.audience);
-});
-builder.Services.AddTransient<EmailService>(provider =>
-{
-    ILogger? logger = provider.GetService<ILogger>();
-    EmailOptions? emailOptions = builder.Configuration
-        .GetSection(EmailOptions.SERVER_EMAIL)
-        .Get<EmailOptions>();
-    if(emailOptions is null)
-    {
-        logger?.Warning("Email do servidor não especificado");
-        emailOptions = new();
-    }
-    
-    return new(emailOptions.smtpServer, emailOptions.port, 
-        emailOptions.email, emailOptions.password);
-});
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER_AND_USER_ROLE,
-        AuthorizationPolicies.RequireIdentifierAndUserRole);
-    options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER_AND_ADMIN_ROLE,
-        AuthorizationPolicies.RequireIdentifierAndAdminRole);
-    options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER_AND_DEV_ROLE,
-        AuthorizationPolicies.RequireIdentifierAndDevRole);
-});
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    JwtParametersOptions jwtOptions = 
-        builder.Configuration.GetSection(JwtParametersOptions.JWT_PARAMETERS)
-            .Get<JwtParametersOptions>()!;
-    
-    byte[] privateKeyByte = Encoding.ASCII.GetBytes(jwtOptions.privateKey);
-    
-    options.TokenValidationParameters = new()
-    {
-        ValidateIssuer = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidateAudience = true,
-        ValidIssuer = jwtOptions.issuer,
-        ValidAudience = jwtOptions.audience,
-        IssuerSigningKey = new SymmetricSecurityKey(privateKeyByte),
-        ValidAlgorithms = new []{ SecurityAlgorithms.HmacSha256 },
-    };
-});
-builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfiguration>();
+builder.Services.AddApiMetadata();
+builder.Services.AddServicesConfiguration(builder.Configuration);
+builder.Services.ConfigureServices();
 
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-builder.Services.AddScoped<ICursoRepository, CursoRepository>();
+builder.Services.AddStorageServices();
+builder.Services.AddServices();
+builder.Services.AddAuth();
 
-builder.Services.AddScoped<IUsuarioController, UsuarioController>();
-builder.Services.AddScoped<IAuthController, AuthController>();
+builder.Services.AddApiControllers();
+builder.Services.AddApiRepositories();
 
 builder.Services.AddOutputCacheCustom();
 
---
+++ StudyLabAPI/Services/Di.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using StudyLabAPI.Context;
+using StudyLabAPI.Controllers;
+using StudyLabAPI.Middlewares.Auth;
+using StudyLabAPI.Middlewares.Swagger;
+using StudyLabAPI.Models.Options;
+using StudyLabAPI.Repositories;
+using StudyLabAPI.Services.Email;
+using StudyLabAPI.Services.Jwt;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+// ReSharper disable UnusedMethodReturnValue.Global
+
+namespace StudyLabAPI.Services;
+
+public static class Di
+{
+    public static IServiceCollection AddServicesConfiguration(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.Configure<JwtParametersOptions>(
+            configuration.GetSection(JwtParametersOptions.JWT_PARAMETERS));
+        services.Configure<EmailOptions>(configuration.GetSection(EmailOptions.SERVER_EMAIL));
+        services.Configure<ConnectionStringsOptions>(configuration
+            .GetSection(ConnectionStringsOptions.CONNECTION_STRING));
+        
+        return services;
+    }
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfiguration>();
+        services.AddTransient<IConfigureOptions<JwtBearerOptions>, AuthenticationJwtBearerConfiguration>();
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddApiMetadata(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+    
+    public static IServiceCollection AddStorageServices(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>();
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<JwtService>();
+        services.AddTransient<EmailService>();
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER_AND_USER_ROLE,
+                AuthorizationPolicies.RequireIdentifierAndUserRole);
+            options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER_AND_ADMIN_ROLE,
+                AuthorizationPolicies.RequireIdentifierAndAdminRole);
+            options.AddPolicy(AuthorizationPolicies.REQUIRE_IDENTIFIER_AND_DEV_ROLE,
+                AuthorizationPolicies.RequireIdentifierAndDevRole);
+        });
+        
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer();
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddApiRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+        services.AddScoped<ICursoRepository, CursoRepository>();
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddApiControllers(this IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioController, UsuarioController>();
+        services.AddScoped<IAuthController, AuthController>();
+        
+        return services;
+    }
+}
