784eb0c610c64873682fddec7844f09db65fb855
LuanRoger <luan.roger.2003@gmail.com>
2023-10-25 15:19:00 -0300

    rafactor: change some parameters from read models and update mappers

--- StudyLabAPI/Mapper/UsuarioModelMapper.cs
+++ StudyLabAPI/Mapper/UsuarioModelMapper.cs
@@ -6,18 +6,14 @@ namespace StudyLabAPI.Mapper;
 [Mapper]
 public partial class UsuarioModelMapper
 {
+    [UseMapper]
+    private readonly CursoModelMapper _cursoModelMapper = new();
+    
     [MapProperty(nameof(UsuarioModel.idUsuario), nameof(UserReadModel.id))]
     [MapProperty(nameof(UsuarioModel.nomeUsuario), nameof(UserReadModel.username))]
     [MapProperty(nameof(UsuarioModel.emailUsuario), nameof(UserReadModel.email))]
     [MapProperty(nameof(UsuarioModel.tipoUsuario), nameof(UserReadModel.role))]
     [MapProperty(nameof(UsuarioModel.statusUsuario), nameof(UserReadModel.active))]
+    [MapProperty(nameof(UsuarioModel.imagemUsuario), nameof(UserReadModel.imagem))]
     public partial UserReadModel UsuarioModelToUserReadModel(UsuarioModel model);
-    
-    //TODO: REVIEW: Move para outro mapper?
-    [MapProperty(nameof(RegisterUserRequestModel.username), nameof(UsuarioModel.nomeUsuario))]
-    [MapProperty(nameof(RegisterUserRequestModel.email), nameof(UsuarioModel.emailUsuario))]
-    [MapProperty(nameof(RegisterUserRequestModel.password), nameof(UsuarioModel.senhaUsuario))]
-    [MapProperty(nameof(RegisterUserRequestModel.role), nameof(UsuarioModel.tipoUsuario))]
-    [MapProperty(nameof(RegisterUserRequestModel.username), nameof(UsuarioModel.nomeUsuario))]
-    public partial UsuarioModel RegisterUserRequestModelToUsuarioModel(RegisterUserRequestModel model);
 }
