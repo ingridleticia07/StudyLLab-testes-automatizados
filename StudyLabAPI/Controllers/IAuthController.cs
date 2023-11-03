6f109134924eb4c27cba2bea888826e5898820c9
LuanRoger <luan.roger.2003@gmail.com>
2023-11-03 15:34:13 -0300

    docs(controller): create doc for user email confirmation and password reset on `IAuthController`

--- StudyLabAPI/Controllers/IAuthController.cs
+++ StudyLabAPI/Controllers/IAuthController.cs
@@ -2,6 +2,7 @@ using StudyLabAPI.Exceptions;
 using StudyLabAPI.Models;
 using StudyLabAPI.Repositories;
 using StudyLabAPI.Services.Jwt;
+using StudyLabAPI.Validators;
 
 namespace StudyLabAPI.Controllers;
 
@@ -16,9 +17,10 @@ namespace StudyLabAPI.Controllers;
 /// </remarks>
 public interface IAuthController
 {
+    //TODO: Documentar também as exceções que cada metodo pode jogar
     /// <summary>
     /// Cadastra um novo usuario no sistema.
-    /// Os campos recebidos em <paramref name="registerUserRequestModel"/> são validados de acordo com //TODO: Validation.
+    /// Os campos recebidos em <paramref name="registerUserRequestModel"/> são validados de acordo com<see cref="RegisterUserRequestModelValidator"/>.
     /// O ID do curso recebido em <paramref name="registerUserRequestModel"/> é usado para relacionar o usuário com o curso,
     /// portanto o curso com este ID deve já estar cadastrado.
     /// </summary>
@@ -30,7 +32,7 @@ public interface IAuthController
     public Task<(UserReadModel, string)> RegisterNewUser(RegisterUserRequestModel registerUserRequestModel);
     /// <summary>
     /// Realiza o login de um usuário já cadastrado no sistema.
-    /// Os campos recebidos em &lt;paramref name="registerUserRequestModel"/&gt; são validados de acordo com //TODO: Validation.
+    /// Os campos recebidos em &lt;paramref name="registerUserRequestModel"/&gt; são validados de acordo com <see cref="UserLoginRequestModelValidator"/>
     /// Nenhuma mudança no banco é feita, apenas resgata as informações do usuário, faz verificações de segurança
     /// e gera um JWT válido.
     /// </summary>
@@ -38,10 +40,34 @@ public interface IAuthController
     /// <returns>Representa uma tarefa assíncrona, ela retorna uma tupla com as informações referentes ao usuário
     /// e um JWT válido</returns>
     public Task<(UserReadModel, string)> LoginUser(UserLoginRequestModel userLoginRequestModel);
+    /// <summary>
+    /// Usa o código de confirmação em <paramref name="confirmUserEmailRequestModel"/> para confirmar o email,
+    /// se o código não for valido, a confirmação não acontecerá.
+    /// A requisição será validada de acordo com <see cref="ConfirmUserEmailRequestModelValidator"/>.
+    /// </summary>
+    /// <param name="confirmUserEmailRequestModel">Informações para confirmação</param>
+    /// <param name="userId">ID de um usuário já cadastrado, o codigo também tem que ser referente a este usuário</param>
+    /// <returns>Representa uma tarefa assíncrona, ela retorna informações sobre a confirmação caso aconteça</returns>
     public Task<CodigoUsuarioReadModel> ConfirmUserEmail(ConfirmUserEmailRequestModel confirmUserEmailRequestModel,
         int userId);
+    /// <summary>
+    /// Usa o codigo de recuperação de senha, juntamente com a senha antiga para alterar a senha do usuário.
+    /// Ela só será alterada caso a o código seja válido e a senha antiga corresponda a senha do usuário.
+    /// A requisição será validada de acordo com <see cref="ResetUserPasswordRequestModelValidator"/>.
+    /// </summary>
+    /// <param name="resetUserPasswordRequestModel">Informações para a redefinição da senha do usuário</param>
+    /// <param name="userId">ID de um usuário já cadastrado, o codigo também tem que ser referente a este usuário</param>
+    /// <returns>Representa uma tarefa assíncrona, ela retorna informações sobre a redefinição da senha do usuário
+    /// caso aconteça</returns>
     public Task<ResetUserPasswordReadModel> ResetUserPassword(ResetUserPasswordRequestModel resetUserPasswordRequestModel,
         int userId);
+    /// <summary>
+    /// Cria e envia um email de recuperação de senha para o usuário, contendo o código de recuperação.
+    /// O antigo código será invalidado e não será possivel usá-lo, ao invés disso, um novo código será gerado.
+    /// </summary>
+    /// <param name="userId">ID de um usuário já cadastrado</param>
+    /// <returns>Representa uma tarefa assíncrona, ela retorna <c>true</c> se o email foi enviado com sucesso,
+    /// caso contrario, ela vai retornar <c>false</c></returns>
     public Task<bool> RequestPasswordResetCode(int userId);
     public Task<bool> RequestConfirmationCode(int userId);
 }
