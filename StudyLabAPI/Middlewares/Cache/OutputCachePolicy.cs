e4f0f63572566128a091f1bdbb6d9bad4abea59e
ROG <luan.roger.2003@gmail.com>
2024-03-15 10:46:02 -0300

    feat: add CORS and remove output cache
    - The reaseon that output cache was removed is that this type of cache is not scalable

--- StudyLabAPI/Middlewares/Cache/OutputCachePolicy.cs
+++ StudyLabAPI/Middlewares/Cache/OutputCachePolicy.cs
@@ -4,10 +4,10 @@ namespace StudyLabAPI.Middlewares.Cache;
 
 public static class OutputCachePolicy
 {
-    public const string USER_GET_USER_BY_ID_POLICY = "user_get_userById";
-    public const string NO_AUTH_REQUIRED_POLICY = "no_auth_required";
+    private const string USER_GET_USER_BY_ID_POLICY = "user_get_userById";
+    private const string NO_AUTH_REQUIRED_POLICY = "no_auth_required";
     
-    public static IServiceCollection AddOutputCacheCustom(this IServiceCollection services)
+    public static IServiceCollection AddCustomOutputCache(this IServiceCollection services)
     {
         services.AddOutputCache(options =>
         {
--- StudyLabAPI/Program.cs
+++ StudyLabAPI/Program.cs
@@ -1,6 +1,6 @@
 using Serilog;
 using StudyLabAPI.Endpoints;
-using StudyLabAPI.Middlewares.Cache;
+using StudyLabAPI.Middlewares.Cors;
 using StudyLabAPI.Middlewares.Filters;
 using StudyLabAPI.Services;
 using ILogger = Serilog.ILogger;
@@ -12,13 +12,15 @@ ILogger serilog = new LoggerConfiguration()
     .Enrich.WithEnvironmentName()
     .WriteTo.Console()
     .CreateLogger();
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog(serilog);
+builder.Logging.ClearProviders()
+    .AddSerilog(serilog);
 builder.Host.UseSerilog(serilog);
 
-builder.Services.AddApiMetadata();
-builder.Services.AddServicesConfiguration(builder.Configuration)
-    .ConfigureServices();
+builder.Services
+    .AddApiMetadata()
+    .AddServicesConfiguration(builder.Configuration)
+    .ConfigureServices()
+    .AddCustomCors();
 
 builder.Services.AddStorageServices()
     .AddLocalServices()
@@ -27,46 +29,47 @@ builder.Services.AddStorageServices()
     .AddApiControllers()
     .AddApiRepositories();
 
-builder.Services.AddAuth()
-    .AddOutputCacheCustom();
+builder.Services.AddAuth();
 
 WebApplication app = builder.Build();
 
-app.UseOutputCache();
-
 if(app.Environment.IsDevelopment())
 {
-    app.UseStaticFiles();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseCors()
+    .UseAuthentication()
+    .UseAuthorization();
 
 RouteGroupBuilder authGroup = app.MapGroup("auth")
     .AddEndpointFilter<ApiKeyFilter>()
+    .RequireCors(CorsPoliciesName.ALLOW_ALL_CORS_POLICY)
     .WithTags("Autenticação")
     .AllowAnonymous();
 authGroup.MapAuthEndpoints();
 
 RouteGroupBuilder userGroup = app.MapGroup("user")
     .AddEndpointFilter<ApiKeyFilter>()
+    .RequireCors(CorsPoliciesName.ALLOW_ALL_CORS_POLICY)
     .WithTags("Usuário");
 userGroup.MapUserEndpoints();
 
 RouteGroupBuilder disciplinaGroup = app.MapGroup("disciplina")
     .AddEndpointFilter<ApiKeyFilter>()
+    .RequireCors(CorsPoliciesName.ALLOW_ALL_CORS_POLICY)
     .WithTags("Disciplina");
 disciplinaGroup.MapDisciplinaEndpoints();
 
 RouteGroupBuilder forumGroup = app.MapGroup("forum")
     .AddEndpointFilter<ApiKeyFilter>()
+    .RequireCors(CorsPoliciesName.ALLOW_ALL_CORS_POLICY)
     .WithTags("Forum");
 forumGroup.MapForumEndpoints();
 
 RouteGroupBuilder materialGroup = app.MapGroup("material")
     .AddEndpointFilter<ApiKeyFilter>()
+    .RequireCors(CorsPoliciesName.ALLOW_ALL_CORS_POLICY)
     .WithTags("Material");
 materialGroup.MapMaterialEndpoints();
 
