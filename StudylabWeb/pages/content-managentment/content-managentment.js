276783d65ac12b7497e24a533eae34b6b847605f
Gustavo B. de Souza <godmulack@gmail.com>
2024-04-05 12:10:13 -0300

    Gerenciamento de conteúdos
    oque eu fiz até agora, tem que finalizar alguns detalhes mas tamo andando; vou subir nessa msm então pra juntar com a main vai ter q estar os dois prontos mb

---
+++ StudylabWeb/pages/content-managentment/content-managentment.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Selecionar todos os checkboxes quando o checkbox do cabeçalho é marcado
+    const selectAllCheckbox = document.getElementById('select-all');
+    selectAllCheckbox.addEventListener('change', function() {
+      const userCheckboxes = document.querySelectorAll('.content-table tbody input[type="checkbox"]');
+      userCheckboxes.forEach((checkbox) => {
+        checkbox.checked = this.checked;
+      });
+    });
+  });
+
+
