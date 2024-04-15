adb738a88562137bab12b95c0b4a03a2d9c11b98
ROG <luan.roger.2003@gmail.com>
2024-04-15 08:13:06 -0300

    test: update services dependencies to use `EnvironmentService`

---
+++ StudyLabAPI/StudyLabAPI.Test/ControllersTests/UtilsControllerTests/UtilsControllerFakeData.cs
@@ -0,0 +1,18 @@
+﻿using StudyLabAPI.Services;
+
+namespace StudyLabAPI.Test.ControllersTests.UtilsControllerTests;
+
+public static class UtilsControllerFakeData
+{
+    public static EnvironmentService fakeEnvironmentService => new()
+    {
+        apiKey = "apiKey",
+        jwtKey = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+        passwordSalt = "salt",
+        smtpEmail = string.Empty,
+        smtpPassword = string.Empty,
+        smtpPort = string.Empty,
+        postgresConnectionString = string.Empty,
+        smtpServer = string.Empty
+    };
+}
