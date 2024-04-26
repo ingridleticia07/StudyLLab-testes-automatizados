bfab3c2522bd290d3bd64d50ac2e92953a948e0f
Gustavo B. de Souza <godmulack@gmail.com>
2024-04-26 11:24:32 -0300

    1º modal

--- StudylabWeb/pages/content-managentment/content-managentment.js
+++ StudylabWeb/pages/content-managentment/content-managentment.js
@@ -1,3 +1,38 @@
+var modal = document.getElementById("modal-excluir");
+var closeButton = document.getElementById("fechar");
+var deleteButton = document.querySelectorAll(".deletar");
+var cancelButton = document.getElementById("cancelarButton");
+var confirmButton = document.getElementById("confirarmButton");
 
 
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+deleteButton.forEach(function(botao) {
+    botao.addEventListener("click", function() {
+        console.log("Botão deletar clicado!");
+        openModal(0)
+    });
+});
+
+modal.onclick = function() {
+    closeModal();
+}
+
+closeButton.onclick = function() {
+  closeModal();
+};
+
+cancelButton.onclick = function() {
+  closeModal();
+};
+
+confirmButton.onclick = function() {
+    closeModal();
+}
 
