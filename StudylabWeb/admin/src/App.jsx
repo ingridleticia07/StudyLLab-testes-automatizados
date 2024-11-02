1b2ccfd18b3b45d579fa8a530a5cc018e0bc11d1
douglas lima <douglaslima13bezerra@gmail.com>
2024-11-01 21:30:46 -0300

    feat(admin): initial setup

---
+++ StudylabWeb/admin/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+    return (
+        <div>
+            <main>
+                <Routes>
+                    <Route path='/' />
+                </Routes>
+            </main>
+        </div>
+    );
+}
+
+export default App;
---
+++ StudylabWeb/admin/src/assets/assets.js
@@ -0,0 +1,53 @@
+import at from './svg/at.svg';
+import arrowLeft from './svg/arrowLeft.svg';
+import arrowRight from './svg/arrowRight.svg';
+import book from './svg/book.svg';
+import check from './svg/check.svg';
+import deleteIcon from './svg/delete.svg';
+import edit from './svg/edit.svg';
+import eyeClose from './svg/eyeClose.svg';
+import eyeOpen from './svg/eyeOpen.svg';
+import filter from './svg/filter.svg';
+import folder from './svg/folder.svg';
+import help from './svg/help.svg';
+import home from './svg/home.svg';
+import info from './svg/info.svg';
+import lock from './svg/lock.svg';
+import notification from './svg/notification.svg';
+import padlock from './svg/padlock.svg';
+import pencil from './svg/pencil.svg';
+import search from './svg/search.svg';
+import user from './svg/user.svg';
+import userSolid from './svg/userSolid.svg';
+
+import logo from './img/StudyLab.svg';
+import team from './img/team.svg';
+
+export const icons = {
+    at,
+    arrowLeft,
+    arrowRight,
+    book,
+    check,
+    deleteIcon,
+    edit,
+    eyeClose,
+    eyeOpen,
+    filter,
+    folder,
+    help,
+    home,
+    info,
+    lock,
+    notification,
+    padlock,
+    pencil,
+    search,
+    user,
+    userSolid,
+};
+
+export const images = {
+    logo,
+    team,
+};
