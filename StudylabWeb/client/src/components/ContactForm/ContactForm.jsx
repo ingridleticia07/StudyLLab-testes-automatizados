752baa21aea34516aea145904b345db3cabff70b
Mulack <godmulack@gmail.com>
2024-12-08 17:10:53 -0300

    Pagina de contato v.2
    agr é para estar certo

--- StudylabWeb/client/src/components/ContactForm/ContactForm.jsx
+++ StudylabWeb/client/src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,60 @@
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(null); 
+
+  const validateEmail = (value) => {
+    const allowedDomains = ["@alu.ufc.br", "@ufc.br"];
+    const isValidDomain = allowedDomains.some((domain) =>
+      value.endsWith(domain)
+    );
+    setIsValid(value.length > 0 ? isValidDomain : null);
+    setEmail(value);
+  };
+
   return (
     <div className="flex justify-center items-center">
-      <form className="bg-white p-6 rounded-lg border-[0.2px] border-americanOrange-500 w-[38rem]">
+      <form className="bg-white p-6 rounded-lg border-[0.2px] border-americanOrange-500 w-[34rem]">
         <div className="mb-4">
-          <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
-            Seu nome
-          </label>
+          <label htmlFor="name" className="block text-gray-700 font-medium mb-2">Seu nome</label>
           <input
             type="text"
             id="name"
             placeholder="Nome completo"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-0"
           />
         </div>
+        
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
             Seu e-mail
           </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="@ Seu e-mail"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
-          />
+          <div className="relative">
+            <span className={`absolute inset-y-0 left-3 flex items-center ${
+                isValid === null
+                  ? "text-gray-500"
+                  : isValid
+                  ? "text-[#00BDEB]" 
+                  : "text-[#EB0000]" 
+              }`}>@</span>
+
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => validateEmail(e.target.value)}
+              placeholder="Seu e-mail"
+              className={`w-full pl-10 px-4 py-2 border rounded-lg focus:outline-none focus:ring-0 ${
+                isValid === null
+                  ? "border-gray-300"
+                  : isValid
+                  ? "border-[#00BDEB]" 
+                  : "border-[#EB0000]" 
+              }`}/>
+          </div>
         </div>
+
         <div className="mb-4">
           <label htmlFor="message" className="block text-gray-700 font-medium mb-2">
             Sua mensagem
@@ -33,15 +63,15 @@ function ContactForm() {
             id="message"
             placeholder="Sugestões, ideias...."
             rows="4"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-0" 
           ></textarea>
         </div>
+
         <button
           type="submit"
-          className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition-colors"
-        >
-          Enviar mensagem
-        </button>
+          disabled={isValid === false}
+          className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+        >Enviar mensagem</button>
       </form>
     </div>
   );
--- StudylabWeb/client/src/pages/Contact.jsx
+++ StudylabWeb/client/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className="bg-white min-h-screen min-w-max w-full mx-auto flex flex-col justify-between items-center">
             <main className="flex flex-col items-center min-w-full flex-grow w-full">
                 <Navbar />
-                <div className="flex items-center justify-between w-full max-w-screen-xl pt-16 pb-28">
+                <div className="flex items-center justify-between w-[1200px] max-w-screen-xl pt-16 pb-28">
                     <h1 className="text-5xl text-americanOrange-400 text-left mt-10">Entre em contato com <br /> nossa equipe</h1>
                     <ContactForm />
                 </div>
