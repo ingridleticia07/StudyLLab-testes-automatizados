c63423c24e96823288c3add7910e696c136dacd1
Mulack <godmulack@gmail.com>
2024-12-11 11:23:52 -0300

    ajuste no botõa
    O botão apenas estava clicando no texto,

--- StudylabWeb/client/src/components/HomeContent/HomeContent.jsx
+++ StudylabWeb/client/src/components/HomeContent/HomeContent.jsx
@@ -5,9 +5,10 @@ const HomeContent = ({ infoText }) => {
       <section className="text-center py-16">
         <h2 className="text-5xl text-gray-600 mb-6">{infoText[0]}</h2>
         <p className="text-gray-600 mb-8 whitespace-pre-line">{infoText[1]}</p>
-        <button className="mt-8 border-2 border-americanOrange-500 bg-transparent text-americanOrange-500 px-4 py-4 rounded-full hover:bg-americanOrange-500 hover:text-white transition">
-          <Link to={'/sobre'}>Quero conhecer</Link>
-        </button>
+        <Link
+          to="/sobre"
+          className="mt-4 border-2 text-xl border-americanOrange-500 bg-transparent text-americanOrange-500 px-4 py-4 rounded-full hover:bg-americanOrange-500 hover:text-white transition text-center inline-block"
+        >Quero conhecer</Link>
       </section>
     );
 };
