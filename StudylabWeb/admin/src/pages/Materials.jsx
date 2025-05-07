6ea7df00541b4d63a477d625dbc717e99ba3ec9c
gabriel <gabriel@DESKTOP-NJTVS4S>
2025-05-07 08:28:16 -0300

    feat: integração listagem de conteúdos na tela de dashboard

--- StudylabWeb/admin/src/pages/Materials.jsx
+++ StudylabWeb/admin/src/pages/Materials.jsx
@@ -1,12 +1,27 @@
-import { useContext } from 'react';
+import { useContext,useEffect,useState } from 'react';
 import { StudylabContext } from '../context/StudylabContext';
 
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
 import TableMaterials from '../components/Tables/TableMaterials';
 import Filter from '../components/Filter/Filter';
+import { getMaterialByDisciplinaOrTopico } from "../../../platform/repository/material";
 
 const Materials = () => {
-    const { data, removeItem } = useContext(StudylabContext);
+    const [showRegister, setShowRegister] = useState(false);
+    const { data, removeItem } = useState();
+    const [conteudo, setConteudo] = useState([]);
+
+    useEffect(() => {
+            const getAllConteudos = async () => {
+                try {
+                    let conteudoList = await getMaterialByDisciplinaOrTopico(1,10, 0,0);
+                    setConteudo(conteudoList);                    
+                } catch (error) {
+                    console.log(error);            
+                }
+            }
+            getAllConteudos();
+        }, []);
 
     return (
         <div className='flex flex-col h-full'>
@@ -14,9 +29,9 @@ const Materials = () => {
             <section className='flex flex-col h-full min-h-[450px] px-3 mb-4 bg-white rounded-lg '>
                 <div className='flex items-center gap-x-10'>
                     <h1 className='py-8 text-3xl font-bold'>Conteúdos</h1>
-                    <Filter data={data.conteudos} />
+                    <Filter data={conteudo.documentos} />
                 </div>
-                <TableMaterials data={data.conteudos} handleDelete={removeItem} />
+                <TableMaterials data={conteudo} handleDelete={removeItem} />
             </section>
         </div>
     );
