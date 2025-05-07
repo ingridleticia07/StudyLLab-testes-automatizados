fcb4942816ab4b935288f436e8e4f7e5d924941a
gabriel <gabriel@DESKTOP-NJTVS4S>
2025-05-07 16:46:36 -0300

    feat: inserindo modal de cadastrar conteúdo em tela de conteúdo

--- StudylabWeb/admin/src/pages/Materials.jsx
+++ StudylabWeb/admin/src/pages/Materials.jsx
@@ -1,9 +1,11 @@
 import { useContext,useEffect,useState } from 'react';
 import { StudylabContext } from '../context/StudylabContext';
+import Button from '../components/Buttons/Button';
 
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
 import TableMaterials from '../components/Tables/TableMaterials';
 import Filter from '../components/Filter/Filter';
+import RegisterMaterial from '../components/RegisterMaterial/RegisterMaterial';
 import { getMaterialByDisciplinaOrTopico } from "../../../platform/repository/material";
 
 const Materials = () => {
@@ -26,6 +28,15 @@ const Materials = () => {
     return (
         <div className='flex flex-col h-full'>
             <Breadcrumb page='Conteúdos' />
+            <section className='rounded-xl bg-white px-4'>
+                <div className='flex items-center justify-between px-6 py-6'>
+                    <h1 className='text-3xl font-bold'></h1>
+                    <Button
+                        text={'Cadastrar Conteúdo'}
+                        handleClick={() => setShowRegister(true)}
+                    />
+                </div>
+            </section>
             <section className='flex flex-col h-full min-h-[450px] px-3 mb-4 bg-white rounded-lg '>
                 <div className='flex items-center gap-x-10'>
                     <h1 className='py-8 text-3xl font-bold'>Conteúdos</h1>
@@ -33,6 +44,9 @@ const Materials = () => {
                 </div>
                 <TableMaterials data={conteudo} handleDelete={removeItem} />
             </section>
+            {showRegister && (
+                <RegisterMaterial handleCancel={() => setShowRegister(false)} />
+            )}
         </div>
     );
 };
