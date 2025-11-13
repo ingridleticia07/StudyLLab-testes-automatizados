b6554dec993dfb205d7a32dc776ca2d36b5696e2
Pedro Lucas <pedrogmobile2@gmail.com>
2025-11-13 14:37:06 -0300

    refactor: aplica pequenos ajustes na HomeContent para deixar a aparência mais parecida com a original em telas maiores

--- StudylabWeb/client/src/components/HomeContent/HomeContent.jsx
+++ StudylabWeb/client/src/components/HomeContent/HomeContent.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 const HomeContent = ({ infoText }) => {
   return (
     <section className="py-16 max-w-xs sm:max-w-xl md:max-w-2xl xl:max-w-6xl mx-auto text-center overflow-hidden">
-      <h2 className="text-2xl sm:text-4xl md:text-6xl text-gray-600 mb-6">{infoText[0]}</h2>
-      <p className="mt-2 text-base sm:text-xl md:text-2xl text-gray-600 mb-8 leading-relaxed">{infoText[1]}</p>
+      <h2 className="text-2xl sm:text-4xl md:text-5xl text-gray-600 mb-6">{infoText[0]}</h2>
+      <p className="mt-2 text-sm sm:text-base text-gray-600 mb-8 whitespace-pre-line">{infoText[1]}</p>
       <Link
         to="/sobre"
-        className="mt-4 border-2 text-lg sm:text-xl lg:text-2xl border-americanOrange-500 bg-transparent text-americanOrange-500 px-4 py-4 rounded-full hover:bg-americanOrange-500 hover:text-white transition text-center inline-block"
+        className="mt-4 border-2 text-lg sm:text-xl border-americanOrange-500 bg-transparent text-americanOrange-500 px-4 py-4 rounded-full hover:bg-americanOrange-500 hover:text-white transition text-center inline-block"
       >Quero conhecer</Link>
     </section>
   );
