7960c127756ec588d426919534b6e1ed92f72d85
Pedro Lucas <pedrogmobile2@gmail.com>
2025-11-14 16:52:06 -0300

    refactor: ajusta layout e responsividade dos componentes HomeCard e HomeFeaturesSection

--- StudylabWeb/client/src/components/Cards/HomeCard.jsx
+++ StudylabWeb/client/src/components/Cards/HomeCard.jsx
@@ -1,14 +1,13 @@
 const HomeCard = ({ info }) => {
-    return (
-      <div className="bg-white rounded-2xl shadow-md p-4 max-w-56 mx-auto relative">
-        <div className="w-9 h-9 bg-gray-300 rounded-full absolute top-4 left-4"></div>
-        <div className="mt-12">
-          <h3 className="text-xl font-semibold text-gray-600 mb-2">{info[0]}</h3>
-          <p className="text-sm text-gray-600">{info[1]}</p>
-        </div>
+  return (
+    <div className="bg-white rounded-2xl shadow-md p-4 max-w-44 max-h-[90vw] md:max-w-56 mx-auto relative">
+      <div className="w-9 h-9 bg-gray-300 rounded-full absolute top-4 left-4"></div>
+      <div className="mt-12">
+        <h3 className="text-lg md:text-xl font-semibold text-gray-600 mb-2">{info[0]}</h3>
+        <p className="text-xs md:text-sm text-gray-600">{info[1]}</p>
       </div>
-    );
-  };
-  
-  export default HomeCard;
-  
+    </div>
+  );
+};
+
+export default HomeCard;
--- StudylabWeb/client/src/components/HomeFeaturesSection/HomeFeaturesSection.jsx
+++ StudylabWeb/client/src/components/HomeFeaturesSection/HomeFeaturesSection.jsx
@@ -3,11 +3,11 @@ import {images} from "../../assets/assets";
 
 const HomeFeaturesSection = () => {
   return (
-    <section className="relative bg-americanOrange-500 rounded-2xl mb-8 mt-4 max-w-[90%] w-[1150px] mx-auto">
-      <div className="grid gap-8 px-16 py-24">
+    <section className="relative bg-americanOrange-500 rounded-2xl mb-3 mt-2 md:mb-8 md:mt-4 w-full max-w-80 xs:max-w-md sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-[1150px] mx-auto">
+      <div className="grid gap-8 px-4 py-6 xl:px-16 xl:py-24">
         
-        <div className="flex flex-col items-start gap-8">
-          <div className="flex gap-8 items-start">
+        <div className="flex flex-col lg:items-start gap-8">
+          <div className="flex gap-6 md:gap-8 items-start">
             <HomeCard 
               info={["Nome da vantagem", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."]}
             />
@@ -15,7 +15,7 @@ const HomeFeaturesSection = () => {
               info={["Nome da vantagem", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."]}
             />
           </div>
-          <div className="flex gap-8 itens-end justify-end pl-10">
+          <div className="flex gap-6 md:gap-8 items-end lg:pl-10">
             <HomeCard 
               info={["Nome da vantagem", "Lorem Ipsum is simply dummy text of the printing and typesetting industry."]}
             />
@@ -25,11 +25,11 @@ const HomeFeaturesSection = () => {
           </div>
         </div>
 
-        <div className="absolute right-[-50px] w-1/2 flex justify-center">
+        <div className="hidden lg:absolute lg:top-1/2 -translate-y-1/2 xl:top-auto xl:translate-y-0 lg:right-[-35px] xl:right-[-50px] lg:w-[calc(56rem/2.4)] xl:w-[calc(1150px/2)] lg:flex xl:justify-center">
           <img
             src={images.laptop}
             alt="Laptop"
-            className="max-w-[90%] md:max-w-full h-auto"
+            className="md:max-w-full h-auto"
           />
         </div>
       </div>
