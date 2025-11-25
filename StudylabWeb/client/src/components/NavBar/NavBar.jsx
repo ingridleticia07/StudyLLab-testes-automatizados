cbf75c49a5f17b1752d695a70c06ff4939dd6ec7
K4ioedu <fontenelekaio33@gmail.com>
2025-11-25 20:33:22 -0300

    refactor: ajustando responsividade do menu da Navbar

--- StudylabWeb/client/src/components/NavBar/NavBar.jsx
+++ StudylabWeb/client/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import NavLink from "../NavLink/NavLink";
 import { useState } from "react";
+import {MdMenu, MdClose} from "react-icons/md";
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div>
-        <nav className="flex justify-between items-center py-4 px-8 bg-white mt-10 md:gap-64 ml-[1vw] mr-[1vw] rounded-full">
+        <nav className="flex justify-between items-center py-4 px-4 md:gap-4 lg:px-8 lg:gap-8 bg-white mt-10  xl:px-10 xl:gap-32  mx-[1vw] rounded-full">
             <div className="text-americanOrange-500 font-urbanist text-[clamp(24px,8vw,64px)]">
                 <Link to="/inicio">Study<strong>Lab</strong></Link>
             </div>
-            <div className=" hidden md:flex items-center space-x-6">
+            <div className=" hidden lg:flex items-center space-x-6 lg:text-sm xl:text-base " >
                 <NavLink to="/inicio" text="Início" link/>
                 <NavLink to="/sobre" text="Sobre" link/>
                 <NavLink to="/contato " text="Contato" link/>
@@ -18,14 +20,14 @@ const Navbar = () => {
                 <NavLink to="/cadastro" text="Criar minha conta" cadastro />
             </div>
 
-            <div className="md:hidden ml-[48vw]" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <div className="lg:hidden ml-[48vw]" onClick={() => setIsMenuOpen(!isMenuOpen)}>
 
-              <button className = "text-3xl ">
-                ☰
+              <button className = "text-4xl ">
+              {isMenuOpen ? <MdClose /> : <MdMenu />}
               </button>
             </div>
         </nav>
-        {isMenuOpen ?(<div className = "md:hidden bg-white w-full shadow-lg">
+        {isMenuOpen ?(<div className = "lg:hidden bg-white w-full shadow-lg">
           <div className = "flex flex-col items-center py-4">
              <NavLink to="/inicio" text="Início" link/>
               <NavLink to="/sobre" text="Sobre" link/>
