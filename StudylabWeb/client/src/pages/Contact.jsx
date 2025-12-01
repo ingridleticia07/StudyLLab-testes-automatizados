63817846b211a8c5a8d47774a75d1adb93eb917e
Pedro Lucas <pedrogmobile2@gmail.com>
2025-12-01 11:49:37 -0300

    refactor: ajusta layout e responsividade do componente Contact e do formulário de contato

--- StudylabWeb/client/src/pages/Contact.jsx
+++ StudylabWeb/client/src/pages/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
         <div className="bg-white min-h-screen min-w-max w-full mx-auto flex flex-col justify-between items-center">
             <main className="flex flex-col items-center min-w-full flex-grow w-full">
                 <Navbar />
-                <div className="flex items-center justify-between w-[1200px] max-w-screen-xl pt-16 pb-28">
-                    <h1 className="text-5xl text-americanOrange-400 text-left mt-10">Entre em contato com <br /> nossa equipe</h1>
+                <div className="flex flex-col lg:flex-row items-center justify-between pt-2 pb-8 lg:pt-16 lg:pb-28 max-w-xs xs:max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-[1200px] gap-8 w-full">
+                    <h1 className="text-2xl sm:text-4xl lg:text-5xl text-americanOrange-400 text-center lg:text-left mt-10 w-full">Entre em contato com <br /> nossa equipe</h1>
                     <ContactForm />
                 </div>
             </main>
