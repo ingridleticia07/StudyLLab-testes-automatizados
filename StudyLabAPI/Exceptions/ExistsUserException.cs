251a6de84f5fab82f12386b50f72da3b381bd0c6
gabriel <gabriel@DESKTOP-NJTVS4S>
2025-12-16 14:34:24 -0300

    feat: alterando msg de exceção de email ou matrícula já existentes

--- StudyLabAPI/Exceptions/ExistsUserException.cs
+++ StudyLabAPI/Exceptions/ExistsUserException.cs
@@ -4,7 +4,7 @@ namespace StudyLabAPI.Exceptions;
 
 public class ExistsUserException : Exception
 {
-    private const string MESSAGE = "Já existe um usuário com a mesma matrícula: [{0}] ou email: [{1}]";
+    private const string MESSAGE = "Já existe um usuário com a matrícula: [{0}] ou o email: [{1}]";
 
     public ExistsUserException(string matricula, string email)
     : base(string.Format(MESSAGE, matricula, email)) { }
